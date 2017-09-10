cf31e3dbfbaaeb4c6cae8860f4104955da8aa5d8
Travis Briggs <audiodude@gmail.com>
2017-09-10 11:49:34 -0700

    Update Question template to include validation.

--- mturk/templates/question.js
+++ mturk/templates/question.js
@@ -1,1 +1,14 @@
-
+$(function() {
+  $('#mturk_form').submit(function() {
+    $('#error-incomplete').hide();
+    // num_lines is passed in from the template.
+    for (var i=0; i < num_lines; i++) {
+      if ($('input[name=accuracy_' + i + ']:checked').length < 1 && $('input[name=unclear_' + i + ']:checked').length < 1) {
+        $('#error-incomplete').show();
+        return false;
+      } else {
+        window.console.log($('input[name=accuracy_' + i + ']:checked').length < 1, $('input[name=unclear_' + i + ']:checked').length < 1)
+      }
+    }
+  });
+});
